a4948879963a183f67463880459ce3a01fd5bb4a
Mohan Sai Potla <potlamohansai@gmail.com>
2022-11-09 16:13:34 +0530

    deployedd

--- src/App.js
+++ src/App.js
@@ -8,31 +8,41 @@ const handler=e=>{
   setInput(e.target.value)
 }
   return (
-    <div><center>
+    <>
+    <h1>Calculator</h1>
+    <div className='outline'><center>
+      
       <input type="text" value={input} name="input"
       onChange={handler}
       /><br/>
       <button onClick={()=>setResult(eval(input))}>Result</button>
-      <h4>Result is : {result}</h4>
-      
+      <div>
       <button onClick={()=>setInput(input+'1')}>1</button>
       <button onClick={()=>setInput(input+'2')}>2</button>
       <button onClick={()=>setInput(input+'3')}>3</button>
       <button onClick={()=>setInput(input+'4')}>4</button>
-      <button onClick={()=>setInput(input+'5')}>5</button><br/>
+      <button onClick={()=>setInput(input+'5')}>5</button>
+      </div>
 
+      <div>
       <button onClick={()=>setInput(input+'6')}>6</button>
       <button onClick={()=>setInput(input+'7')}>7</button>
       <button onClick={()=>setInput(input+'8')}>8</button>
       <button onClick={()=>setInput(input+'9')}>9</button>
       <button onClick={()=>setInput(input+'0')}>0</button><br/>
+      </div>
 
+      <div>
       <button onClick={()=>setInput(input+'+')}>+</button>
       <button onClick={()=>setInput(input+'-')}>-</button>
       <button onClick={()=>setInput(input+'*')}>*</button>
       <button onClick={()=>setInput(input+'/')}>/</button>
       <button onClick={()=>setInput('')}>CLR</button>
+      </div>
+
+      <h4>Result is : {result}</h4>
       </center></div>
+      </>
   )
 }
 
